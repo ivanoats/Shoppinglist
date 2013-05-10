27551211ee0486081d2dce02daf6eb94fff3762c
Andy Reilly <areilly@kentfirerfa.org>
2013-05-09 21:03:55 -0700

    added /coverage to .gitignore

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,15 @@
 
 ActiveRecord::Schema.define(:version => 20130502052933) do
 
+  create_table "lists", :force => true do |t|
+    t.string   "name"
+    t.string   "status"
+    t.string   "category"
+    t.boolean  "publicity"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
     t.string   "encrypted_password",     :default => "", :null => false
